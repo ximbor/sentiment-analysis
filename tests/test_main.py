884ec99b00455d3899547e1a951a8e09e130942a
Simone <simoneb81@gmail.com>
2026-01-01 20:36:46 +0100

    fix: tests imports

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
-from src.main import main as app
+from src.main import app
 
 @pytest.mark.asyncio
 async def test_predict_sentiment_success():
